79e92f9f5a0fe2ca60c548ab431ecfb21d624763
Michael Andrews <mike.andrews357@gmail.com>
2019-01-20 14:00:50 -0500

    Added simple form gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_20_170900) do
+ActiveRecord::Schema.define(version: 2019_01_20_185621) do
+
+  create_table "costs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "age"
+    t.string "babysitter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "hosts", force: :cascade do |t|
     t.string "title"
